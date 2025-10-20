4b4d2eb8a2b33d49f4ef2aef699fafa6ca9cb97e
uzielweb <uzielweb@gmail.com>
2025-10-20 17:02:48 -0300

    🔄 Finalize class name standardization to lowercase
    📝 **Correção final da padronização:**
    - ✅ Arquivo icode_clientes.php criado com nome correto (minúsculo)
    - ✅ Versão atualizada para 1.0.1 no novo arquivo
    - ✅ Classe icode_clientes seguindo padrão Joomla
    - ✅ DatabaseAwareTrait corretamente implementado
    - ✅ setDatabase() método atualizado
    
    🎯 **Benefícios:**
    - Nomenclatura consistente com padrões PSR-4
    - Compatibilidade total com Joomla 6
    - Código mais limpo e organizado
    - Elimina warnings de case-sensitivity
    
    📋 **Status:** Componente totalmente preparado para v1.0.1

---
+++ administrator/src/Service/Html/icode_clientes.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @version    CVS: 1.0.1
+ * @package    Com_Icode_clientes
+ * @author     Rogerio Brito <rogerio.brito@intercode.com.br>
+ * @copyright  2025 Rogerio Brito
+ * @license    GNU General Public License versão 2 ou posterior; consulte o arquivo License. txt
+ */
+
+namespace Icodeclientes\Component\Icode_clientes\Administrator\Service\Html;
+
+// No direct access
+defined('_JEXEC') or die;
+
+use Joomla\Utilities\ArrayHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\Database\DatabaseAwareTrait;
+use Joomla\Database\DatabaseDriver;
+
+/**
+ * Icode_clientes HTML Helper.
+ *
+ * @since  1.0.0
+ */
+class icode_clientes
+{
+	use DatabaseAwareTrait;
+
+	/**
+	 * Public constructor.
+	 *
+	 * @param   DatabaseDriver  $db  The Joomla DB driver object for the site's database.
+	 */
+	public function __construct(DatabaseDriver $db)
+	{
+		$this->setDatabase($db);
+	}
+
+	public function toggle($value = 0, $view='', $field='', $i='')
+	{
+		$states = array(
+			0 => array('icon-unpublish', Text::_('Toggle'), ''),
+			1 => array('icon-publish', Text::_('Toggle'), '')
+		);
+
+		$state  = ArrayHelper::getValue($states, (int) $value, $states[0]);
+		$text   = '<span aria-hidden="true" class="' . $state[0] . '"></span>';
+		$html   = '<a href="javascript:void(0);" class="tbody-icon ' . $state[2] . '"';
+		$html  .= 'onclick="return Joomla.toggleField(\'cb'.$i.'\',\'' . $view . '.toggle\',\'' . $field . '\')" title="' . Text::_($state[1]) . '">' . $text . '</a>';
+
+		return $html;
+	}
+}
